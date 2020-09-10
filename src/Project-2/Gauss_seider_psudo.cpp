1dc62f50a39e2c114e844a7ac94135f9ab03c2ba
FredrikNM <fredrik_nm@hotmail.com>
2020-09-10 12:06:09 +0200

    Create Gauss_seider_psudo.cpp

---
+++ src/Project-2/Gauss_seider_psudo.cpp
@@ -0,0 +1,28 @@
+
+void Gauss seidel(){
+
+  X_new = []
+	X_past = []
+	b = []
+	a = [][]
+
+	while dist(X_new- X_past) > epsilon k++:
+
+		for i in range(len(b)):
+		for (i=0; i < row_length to a; i++){
+
+			for (j = 0; j < column length to a; j++){
+				if (j < i){
+					sum_part_1 = a[i][j]*X_new[i]
+				}
+				if (j > i){
+					sum_part_2 = a[i][j]*X_past[i]
+				}
+			}
+			X_new[i] = b[i] - sum_part_1 - sum_part_2
+
+
+	}
+}
+
+
