bed7bd6460af15cff0c27be27bf361299c97df86
FredrikNM <fredrik_nm@hotmail.com>
2020-09-11 10:02:05 +0200

    Add files via upload

---
+++ src/Project-2/offdiagonal.cpp
@@ -0,0 +1,12 @@
+
+
+void offdiagonal(A, p, q, n){
+	double max = 0
+	for (int = 0 ; i < n ; i++){
+		for (int j = i + 1; j < n; j++){
+			if (fabs(A[i,j] > max)){
+				max = A[i,j]; p = i; q = j;
+			}
+		}
+	}
+}
