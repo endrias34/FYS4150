24e3f2dd28a9ea9b8f379262bf5fba8051e15ac4
FredrikNM <fredrik_nm@hotmail.com>
2020-09-11 10:06:16 +0200

    Update and rename offdiagonal_psudo.cpp to offdiagonal.cpp

--- src/Project-2/offdiagonal.cpp
+++ src/Project-2/offdiagonal.cpp
@@ -1,10 +1,10 @@
 
 
-void offdiagonal(A, p, q, n){
+void offdiagonal(mat A, int p, int q, int n){
 	double max = 0
 	for (int = 0 ; i < n ; i++){
 		for (int j = i + 1; j < n; j++){
-			if (fabs(A[i,j] > max)){
+			if (fabs(A[i,j]) > max){
 				max = A[i,j]; p = i; q = j;
 			}
 		}
