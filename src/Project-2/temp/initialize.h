b7da859d2131c23ddcfdd161924d2d952d49b933
FredrikNM <fredrik_nm@hotmail.com>
2020-09-29 10:20:11 +0200

    Delete initialize.h

--- src/Project-2/temp/initialize.h
+++
@@ -1,8 +0,0 @@
-#ifndef INITIALIZE_H
-#define INITIALIZE_H
-
-void fill_beam_array(arma::mat& A, arma::vec& C1, arma::vec& B, int n);
-void fill_array(arma::mat& A, int n);
-void fill_array_interactive(arma::mat& A, int n);
-
-#endif // INITIALIZE_H
