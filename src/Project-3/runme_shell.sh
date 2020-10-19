da4e3425ed21b1b659f940f10c881f18bd1fb00f
endrias34 <69977258+endrias34@users.noreply.github.com>
2020-10-19 09:20:54 +0200

    Add files via upload

---
+++ src/Project-3/runme_shell.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+# for i in Mercury_GR
+#    do
+#    for j in verlet 
+#      do
+#        for k in 500
+#           do
+#             for l in 5
+#                do
+#                  for m in 1000
+#                  do
+#                     ./runme $i $j $k $l $m  
+#                 done
+#             done 
+#         done
+#     done
+# done
+
+# for i in 3BodyFull #SolarSystem # #3Body
+#    do
+#    for j in verlet 
+#      do
+#        for k in 250
+#           do
+#             for l in 5
+#                do
+#                  for m in 100
+#                  do
+#                     ./runme $i $j $k $l $m  
+#                 done
+#             done 
+#         done
+#     done
+# done
+
+# for i in 2Body
+#    do
+#    for j in verlet 
+#      do
+#        for k in 100
+#           do
+#             for l in 5
+#                do
+#                  for m in 100
+#                  do
+#                     ./runme $i $j $k $l $m  
+#                 done
+#             done 
+#         done
+#     done
+# done
+
+# for i in 2Body
+#    do
+#    for j in verlet #euler #
+#      do
+#        for k in 50 #10
+#           do
+#             for l in 5 #7
+#                do
+#                  for m in 100
+#                  do
+
+#                     #./runme $i $j $k $l $m  > Results/Energy_Euler.txt
+#                     #./runme $i $j $k $l $m  > Results/Energy_Verlet_ellipse_beta3.txt
+#                     #./runme $i $j $k $l $m  > Results/Angmom_Euler.txt
+#                     ./runme $i $j $k $l $m  > Results/Angmom_Verlet_ellipse_beta3.txt
+#                 done
+#             done 
+#         done
+#     done
+# done
+
+
+
+# for i in 2Body
+#    do
+#    for j in verlet #euler #
+#      do
+#        for k in 1
+#           do
+#             for l in 3 4 5 6 7 
+#                do
+#                  for m in 10
+#                  do
+#                     ./runme $i $j $k $l $m  
+#                     #./runme $i $j $k $l $m  
+#                 done
+#             done 
+#         done
+#     done
+# done
+
+
+
+#for i in 2Body
+#  do
+#    for j in verlet euler
+#      do
+#        for k in 1.0
+#           do
+#             for l in 3 4 5 6 7 8
+#                do
+#                 ./runme $i $j $k $l 
+#             done 
+#         done
+#     done
+# done
---
+++ src/Project-3/solver.h
@@ -0,0 +1,55 @@
+
+#ifndef SOLVER_H
+#define SOLVER_H
+
+# include <iostream>
+# include <fstream>
+# include <iomanip>
+# include <string>
+# include <cmath>
+# include <ctime>
+# include "planet.h"
+# include "constants.h"
+
+/* Class to represent an n-body system and its solutions. List of members:
+ * - num_bodies [int]                  --> Number of bodies in simulation
+ * - fixed_sun  [bool]                 --> Whether to use fixed sun or moveable sun
+ * - bodies     [planet*]              --> Array of planets
+ * - ofiles     [doublesrd::ofstream*] --> Array of file objects for output files
+ *
+ * - solve                    [void] --> Manager function for solving n-body system. Also performs tests
+ * - write_row_to_file        [void] --> Writes a row of t, x, y, z values to output file
+ * - euler                    [void] --> Euler algorithm for time stepping the n-body system
+ * - verlet                   [void] --> Velocity Verlet algorithm for time stepping the n-body system
+ * - verlet_GR                [void] --> Specific Velocity Verlet algorithm for mercury GR case
+ * - compute_total_acc        [double] --> Returns total acceleration on one body
+ * - compute_total_acc_GR     [double] --> Specific general relativistic acceleration for mercury case
+ * - compute_energy           [void] --> Prints out the total kinetic, potential and mechanical energy
+ * - compute_angular_momentum [void] --> Prints out the total angular momentum of the system
+ * - compute_center_mass      [void] --> Prints out coordinates of the center of mass for the system */
+
+class solver
+{
+    public:
+        solver();
+        solver(planet* bodies, int n, bool implicit_sun = false);
+        void solve(double h, double t_max, double t_write, std::string method);
+        void write_row_to_file(int file_index, double t, double x, double y, double z);
+        ~solver();
+
+    private:
+        void euler(double h, double t_max, int frame_write);
+        void verlet(double h, double t_max, int frame_write);
+        void verlet_GR(double h, double t_max, int frame_write);
+        double compute_total_acc(planet subject, planet* objects, int dim) const;
+        double compute_total_acc_GR(planet subject, int dim);
+        void compute_energy(double time,int frame_write, int frame) const;
+        void compute_angular_momentum(double time, int frame_write, int frame) const;
+        void compute_center_mass() const;
+        int num_bodies;
+        bool fixed_sun;
+        planet* bodies;
+        std::ofstream* ofiles;
+};
+
+#endif // SOLVER_H
